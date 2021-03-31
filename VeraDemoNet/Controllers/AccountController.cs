bd05d84390695cb3554dda115a55e3cbf47d3733
Siarhei Kuchuk <skuchuk@lms365.com>
2021-03-31 19:08:46 +0300

    12-cwe-384-session-fixation.md

--- VeraDemoNet/Controllers/AccountController.cs
+++ VeraDemoNet/Controllers/AccountController.cs
@@ -14,6 +14,7 @@ using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Mvc;
+using System.Web.SessionState;
 using Newtonsoft.Json;
 using VeraDemoNet.DataAccess;
 using VeraDemoNet.Models;
@@ -118,6 +119,8 @@ namespace VeraDemoNet.Controllers
                         return View(loginViewModel);
                     }
 
+                    RegenerateSessionId();
+                    
                     if (loginViewModel.RememberLogin)
                     {
                         var userModel = new CustomSerializeModel()
@@ -157,6 +160,33 @@ namespace VeraDemoNet.Controllers
 
             return View(loginViewModel);
 
+        }
+
+        private void RegenerateSessionId()
+        {
+            var Context = System.Web.HttpContext.Current;
+            System.Web.SessionState.SessionIDManager manager = new System.Web.SessionState.SessionIDManager();
+            string oldId = manager.GetSessionID(Context);
+            string newId = manager.CreateSessionID(Context);
+            bool isAdd = false, isRedir = false;
+            manager.SaveSessionID(Context, newId, out isRedir, out isAdd);
+            HttpApplication ctx = Context.ApplicationInstance;
+            HttpModuleCollection mods = ctx.Modules;
+            System.Web.SessionState.SessionStateModule ssm = (SessionStateModule)mods.Get("Session");
+            System.Reflection.FieldInfo[] fields = ssm.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            SessionStateStoreProviderBase store = null;
+            System.Reflection.FieldInfo rqIdField = null, rqLockIdField = null, rqStateNotFoundField = null;
+            foreach (System.Reflection.FieldInfo field in fields)
+            {
+                if (field.Name.Equals("_store")) store = (SessionStateStoreProviderBase)field.GetValue(ssm);
+                if (field.Name.Equals("_rqId")) rqIdField = field;
+                if (field.Name.Equals("_rqLockId")) rqLockIdField = field;
+                if (field.Name.Equals("_rqSessionStateNotFound")) rqStateNotFoundField = field;
+            }
+            object lockId = rqLockIdField.GetValue(ssm);
+            if ((lockId != null) && (oldId != null)) store.ReleaseItemExclusive(Context, oldId, lockId);
+            rqStateNotFoundField.SetValue(ssm, true);
+            rqIdField.SetValue(ssm, newId);
         }
 
         [HttpGet, ActionName("Logout")]
