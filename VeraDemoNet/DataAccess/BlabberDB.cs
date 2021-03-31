b611abd7dc1566a331709cdbc7c0afcce890f702
Siarhei Kuchuk <skuchuk@lms365.com>
2021-03-31 17:54:51 +0300

    8-cwe-259-hardcoded-password.md

--- VeraDemoNet/DataAccess/BlabberDB.cs
+++ VeraDemoNet/DataAccess/BlabberDB.cs
@@ -4,12 +4,13 @@ namespace VeraDemoNet.DataAccess
 {
     public class BlabberDB : DbContext  
     {
-        const string DATA_SOURCE = "(localdb)\\MSSQLLocalDB";
-        const string DB_FILENAME = "|DataDirectory|VeraDemoNet.mdf";
-        const string LOGIN = "admin";
-        const string PASSWORD = "admin123";
-        const string OPTIONS = "Integrated Security=True;User Instance=False";
-        public BlabberDB() : base($"Data Source={DATA_SOURCE};AttachDbFilename={DB_FILENAME};User Id={LOGIN};Password={PASSWORD};{OPTIONS}")  
+        //const string DATA_SOURCE = "(localdb)\\MSSQLLocalDB";
+        //const string DB_FILENAME = "|DataDirectory|VeraDemoNet.mdf";
+        //const string LOGIN = "admin";
+        //const string PASSWORD = "admin123";
+        //const string OPTIONS = "Integrated Security=True;User Instance=False";
+        // $"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|VeraDemoNet.mdf;User Id=admin;Password=admin123;Integrated Security=True;User Instance=False"
+        public BlabberDB() : base("BlabberDB")  
         {  
         }  
   
